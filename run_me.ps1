567e3b24350c34253adcdec671e7268f2746e616
fschwiet <fschwiet@gmail.com>
2011-03-10 19:53:16 -0800

    removing usage of psake (it was overkill)

--- run_me.ps1
+++ run_me.ps1
@@ -1,16 +1,112 @@
 
-param($psakeScriptToDraw, $outputDirectory)
+param($psakeScriptToDraw, $outputDirectory = ".\output\")
 
-import-module .\psake.psm1
+function VisualizePSakeScript($sourceDirectory, $psakeScriptToDraw, $outputDirectory) {
 
-$properties = @{};
+    $psakeScriptFileinfo = (New-Object -TypeName "System.IO.FileInfo" -ArgumentList (gi $psakeScriptToDraw))
 
-if ($psakeScriptToDraw) {
-    $properties.psakeScriptToDraw = $psakeScriptToDraw;
+    $tasks = LoadTasks $psakeScriptFileinfo
+    
+    $global:tasks = $tasks
+    
+	$result = DrawTasks $tasks
+	$result
+    
+    $outputFilename = $psakeScriptFileinfo.BaseName;
+    
+    if (-not (test-path $outputDirectory)) {
+        $null = mkdir $outputDirectory
+    }
+    
+    $result | & "$sourceDirectory\Graphviz2.26.3\dot.exe" -Tjpg -o (join-path $outputDirectory "$outputFilename.jpg")
+    $result | & "$sourceDirectory\Graphviz2.26.3\dot.exe" -Tpdf -o (join-path $outputDirectory "$outputFilename.pdf")
 }
 
-if ($outputDirectory) {
-    $properties.outputDirectory = $outputDirectory;
+function DrawTasks {
+    $result = "`
+digraph {`
+    graph [rank=""source"";rankdir = ""LR""];"
+
+    $ks = @($tasks.keys);
+    [Array]::reverse($ks);
+
+    foreach($name in $ks) {
+
+        $task = $tasks[$name];
+        
+        $line = "`n    $name [ shape=""record"", label=<$name> ] ";
+    
+        foreach($dependency in $task.depends) {
+            $line += " $name -> $dependency;"
+        }
+        
+        $result += $line
+    }
+    
+    $result += "`n}`n"
+    $result
 }
 
-.\psake.ps1 .\default.ps1 -properties $properties
+function LoadTasks([System.IO.FileInfo] $psakeScript) {
+
+    $tasks = @{};
+
+    function Include {
+	}
+
+    function Properties {
+    }
+
+    function Task {
+        
+        param(
+            [Parameter(Position=0,Mandatory=1)]
+            [string]$name = $null,
+
+            [Parameter(Position=1,Mandatory=0)]
+            [scriptblock]$action = $null,
+
+            [Parameter(Position=2,Mandatory=0)]
+            [scriptblock]$preaction = $null,
+
+            [Parameter(Position=3,Mandatory=0)]
+            [scriptblock]$postaction = $null,
+
+            [Parameter(Position=4,Mandatory=0)]
+            [scriptblock]$precondition = {$true},
+
+            [Parameter(Position=5,Mandatory=0)]
+            [scriptblock]$postcondition = {$true},
+
+            [Parameter(Position=6,Mandatory=0)]
+            [switch]$continueOnError = $false,
+
+            [Parameter(Position=7,Mandatory=0)]
+            [string[]]$depends = @(),
+
+            [Parameter(Position=8,Mandatory=0)]
+            [string]$description = $null
+        )        
+        
+        $tasks[$name] = @{
+            name = $name;
+            depends = $depends;
+        };
+    }
+    
+    $originalLocation = get-location
+    set-location $psakeScript.Directory
+    
+    try {
+    
+        & (gi $psakeScript)
+    } finally {
+        set-location $originalLocation
+    }
+    
+	$tasks
+}
+
+
+VisualizePSakeScript "." $psakeScriptToDraw $outputDirectory 
+
